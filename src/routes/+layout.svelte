7db5602341f50806b984fdebf6a04aa862a28fa4
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-05-08 20:53:43 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Navbar from '../components/navbar.svelte';
+
+	import './styles.scss';
+	import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+</script>
+
+<main>
+	<slot />
+	<Navbar />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<svelte:head>
+    <title>Emre Can Çorapçı Portfolio</title>
+    https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@200..900&display=swap
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
