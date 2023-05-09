cf6cadd878474f83dca29f7de3d62b1d729826b8
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-05-09 09:54:44 +0300

    navbar implementation

---
+++ src/components/navbar.svelte
@@ -0,0 +1,49 @@
+<div class="row navbar hard-shadow">
+	<div class="col-auto">
+		<a href="/">
+			<img src="/img/Logo.png" alt="Logo - Emre Can">
+        </a>
+	</div>
+	<div class="col" />
+	<div class="col-auto nav hard-shadow">
+		<a href="/" class="text-shadow-sm">Home</a>
+		<a href="/Projects" class="text-shadow-sm">Projects</a>
+		<a href="/About" class="text-shadow-sm">About</a>
+		<a href="/ContactMe" class="text-shadow-sm">Contact Me</a>
+	</div>
+</div>
+
+<style lang="scss">
+	@use 'sass:map';
+	@import 'src/sass/styles.scss';
+
+	.navbar {
+        margin: 0 8rem;
+		padding: 1rem 2rem;
+		background-color: map.get($theme-colors, 'dark');
+		border-radius: 1rem;
+	}
+
+	.nav {
+		padding: 0.5rem 1rem;
+		background-color: map.get($theme-colors, 'lighter');
+		border-radius: 32px 4px;
+
+		a {
+			padding: 0 1rem;
+			color: map.get($theme-colors, 'darker');
+
+            &:hover {
+                color: map.get($theme-colors, 'white');
+            }
+
+            &:active {
+                color: map.get($theme-colors, 'secondary');
+            }
+
+            .active {
+                color: map.get($theme-colors, 'primary');
+            }
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,22 @@
 </script>
 
 <main>
-	<slot />
-	<Navbar />
+	<div class="container">
+		<slot />
+		<Navbar />
+	</div>
 </main>
+
+<style lang="scss">
+	main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+		font-family: 'Space Grotesk', sans-serif;
+		font-style: normal;
+		background-color: #cece86;
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
